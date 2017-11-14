a809d88b93dba062089f9a40eb497bd9f0edb3f0
anil235476 <aks.dce.007@gmail.com>
2017-11-14 22:45:34 +0530

    effective cmake adding

---
+++ effective_cmake/main.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "main.h"
+using namespace std;
+
+int main(){
+	cout<<"hello\n";
+	test t;
+	t.print();
+
+}
---
+++ effective_cmake/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+struct test{
+	void print(){
+		std::cout<<"from test\n";
+	}
+};
+
+#endif//_MAIN_H_
